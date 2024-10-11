fc37b3aae9e0ad0c81218310db499e1017c2fcd7
Alexey Popov <lehaksay@gmail.com>
2024-10-11 17:58:12 +0300

    Add Chinese language

--- hello.pas
+++ hello.pas
@@ -1,8 +1,9 @@
 ﻿begin
-  var language:=ReadInteger('What language do you speak? Write in English.');
+  var language:=ReadInteger('What language do you speak? Write in English with a capital letter.');
   case language of
   'English':Print('Hello');
   'Russian':Print('Привет');
   'French':Print('bonjour à');
   'Italien':Print('ciao');
+  'Chinese':Print('嗨。');
 end.
